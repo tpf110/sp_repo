91335c765613a8d07acfe338e9dabfb4d228a22a
UID <UID@192.168.176.1>
2019-06-27 18:14:25 +0800

    sss

--- sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Sp02ItemserviceApplication {
-
+//sss
 	public static void main(String[] args) {
 		SpringApplication.run(Sp02ItemserviceApplication.class, args);
 	}
